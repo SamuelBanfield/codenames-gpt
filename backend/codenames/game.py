36cd4cd359fc7e09966f1a5a7b41906f5f8e2b81
SamuelBanfield <samueljbanfield@gmail.com>
2024-11-10 19:45:56 +0000

    Actually reject guesses/cclues if the game is over rather than just logging

--- backend/codenames/game.py
+++ backend/codenames/game.py
@@ -61,6 +61,7 @@ class CodenamesGame:
     async def guess_tile(self, user: User, tile: Tile) -> Optional[Literal["red", "blue"]]:
         if self.check_win():
             print("Ignoring guess as game is over")
+            return
         if self.is_user_turn(user) and not user.is_spy_master:
             tile.reveal()
             may_continue: bool = self.update_guesses_remaining(tile, user)
@@ -96,6 +97,7 @@ class CodenamesGame:
     async def provide_clue(self, user: User, word: str, number: int):
         if self.check_win():
             print("Ignoring guess as game is over")
+            return
         if self.is_user_turn(user) and user.is_spy_master:
             self.clue = (word, number)
             self.guesses_remaining = number
